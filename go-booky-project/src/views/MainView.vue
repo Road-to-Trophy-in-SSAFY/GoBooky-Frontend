fc861e3f2a2139bed5478830c4005ad9eb56d0da
Hyeseong Ro <nks3319@gmail.com>
2025-05-23 04:35:18 +0900

    feat: 회원탈퇴 모달 추가 및 비밀번호 입력 기능 구현

--- go-booky-project/src/views/MainView.vue
+++ go-booky-project/src/views/MainView.vue
@@ -4,7 +4,7 @@
     <div v-if="auth.isAuthenticated" class="main-actions">
       <span style="font-weight: bold">{{ auth.user?.email }} 님 환영합니다!</span>
       <button @click="handleLogout">로그아웃</button>
-      <button @click="handleWithdraw">회원탈퇴</button>
+      <button @click="openDeleteModal">회원탈퇴</button>
     </div>
     <div v-else class="main-actions">
       <RouterLink to="/login">
@@ -15,6 +15,21 @@
       </RouterLink>
     </div>
     <Modal v-if="modalText" :text="modalText" @close="modalText = ''" />
+    <Modal v-if="showDeleteModal" @close="closeDeleteModal">
+      <template #default>
+        <div style="margin-bottom: 1rem">정말 계정을 삭제하시겠습니까?</div>
+        <input
+          v-model="deletePassword"
+          type="password"
+          placeholder="비밀번호 입력"
+          style="margin-bottom: 1rem"
+        />
+        <div style="display: flex; gap: 1rem; justify-content: center">
+          <button @click="closeDeleteModal">취소</button>
+          <button @click="handleWithdraw" style="background: #e74c3c">삭제</button>
+        </div>
+      </template>
+    </Modal>
   </div>
 </template>
 
@@ -28,6 +43,8 @@ import axios from '@/services/axios'
 const auth = useAuthStore()
 const router = useRouter()
 const modalText = ref('')
+const showDeleteModal = ref(false)
+const deletePassword = ref('')
 
 function handleError(err, fallbackMsg = '회원탈퇴 실패') {
   modalText.value = err.response?.data?.detail || fallbackMsg
@@ -38,10 +55,24 @@ const handleLogout = () => {
   router.push('/')
 }
 
+function openDeleteModal() {
+  showDeleteModal.value = true
+  deletePassword.value = ''
+}
+function closeDeleteModal() {
+  showDeleteModal.value = false
+  deletePassword.value = ''
+}
+
 const handleWithdraw = async () => {
+  if (!deletePassword.value) {
+    modalText.value = '비밀번호를 입력해주세요.'
+    return
+  }
   try {
-    await axios.delete('/auth/account/', { data: { password: '' } })
+    await axios.delete('/auth/account/', { data: { password: deletePassword.value } })
     auth.logout()
+    showDeleteModal.value = false
     modalText.value = '회원탈퇴가 완료되었습니다.'
     setTimeout(() => {
       router.push('/')
