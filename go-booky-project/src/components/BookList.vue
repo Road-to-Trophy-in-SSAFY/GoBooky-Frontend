3a464048634b57f671d8a85e7676c26793c2f790
김수민 <gkgkgkgk22@naver.com>
2025-05-22 15:21:03 +0900

    feat: BookList 출력 준비 - view, component 생성 및 연결

---
+++ go-booky-project/src/components/BookList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>book-list</p>
+    <BookCard />
+  </div>
+</template>
+
+<script setup>
+import BookCard from '@/components/BookCard.vue'
+</script>
+
+<style scoped>
+/* */
+</style>
---
+++ go-booky-project/src/views/BookListView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>book-list-view</p>
+    <BookList />
+  </div>
+</template>
+
+<script setup>
+import BookList from '@/components/BookList.vue'
+</script>
+
+<style scoped>
+/* */
+</style>
