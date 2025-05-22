eb3e8a869ccaa2e1ca38eb3f75acc5330f5d2e9d
Hyeseong Ro <nks3319@gmail.com>
2025-05-23 04:20:46 +0900

    refactor: 라우터에서 컴포넌트 임포트를 직접 사용하도록 수정

--- go-booky-project/src/router/index.js
+++ go-booky-project/src/router/index.js
@@ -1,25 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import EmailVerificationView from '@/views/auth/EmailVerificationView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
+import MainView from '@/views/MainView.vue'
+import SignupView from '@/views/auth/SignupView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Main',
-    component: () => import('@/views/MainView.vue'),
+    component: MainView,
   },
   {
     path: '/signup',
     name: 'Signup',
-    component: () => import('@/views/auth/SignupView.vue'),
+    component: SignupView,
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('@/views/auth/LoginView.vue'),
+    component: LoginView,
   },
   {
     path: '/verify-email/:uuid',
     name: 'EmailVerification',
-    component: () => import('@/views/auth/EmailVerificationView.vue'),
+    component: EmailVerificationView,
   },
 ]
 
