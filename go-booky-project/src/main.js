cf01b71b010fd8a4c2f529c23b14b8514b8acc28
Hyeseong Ro <nks3319@gmail.com>
2025-05-23 00:06:13 +0900

    feat: Pinia 플러그인 추가 및 초기화 방식 수정

--- go-booky-project/src/main.js
+++ go-booky-project/src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
+// Pinia 생성 및 플러그인 추가
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
